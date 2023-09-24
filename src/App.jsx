da3c144cdced07c4bc3ff4545f466eb2bd3e9367
Paul <pablovanduo@gmail.com>
2023-09-24 15:39:26 +0300

    Adjust navigation routening

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
@@ -9,6 +9,8 @@ import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
+import City from './components/City';
+import Form from './components/Form';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -40,18 +42,14 @@ function App() {
 				<Route path="pricing" element={<Pricing />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
-					<Route
-						index
-						element={
-							<CityList cities={cities} isLoading={isLoading} />
-						}
-					/>
+					<Route index element={<Navigate replace to="cities" />} />
 					<Route
 						path="cities"
 						element={
 							<CityList cities={cities} isLoading={isLoading} />
 						}
 					/>
+					<Route path="cities/:id" element={<City />} />
 					<Route
 						path="countries"
 						element={
@@ -61,7 +59,7 @@ function App() {
 							/>
 						}
 					/>
-					<Route path="form" element={<p>Form</p>} />
+					<Route path="form" element={<Form />} />
 				</Route>
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
